8a991870308c0cf9a94e47b100ae0ec925f1ab9c
Danyl993 <danyl.bince2000@gmail.com>
2026-06-18 22:25:30 +0530

    feat(frontend): create upload panel

---
+++ frontend/src/components/UploadPanel.jsx
@@ -0,0 +1,11 @@
+export default function UploadPanel() {
+  return (
+    <div className="border border-gray-800 rounded p-4">
+      <h2 className="font-bold mb-2">
+        Upload Study Material
+      </h2>
+
+      <input type="file" />
+    </div>
+  );
+}
