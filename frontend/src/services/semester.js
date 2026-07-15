7ad9c1fa0afa6a2c58e833d665ce65afef030184
Danyl993 <danyl.bince2000@gmail.com>
2026-07-15 18:28:06 +0530

    feat: add frontend semester API service

---
+++ frontend/src/services/semester.js
@@ -0,0 +1,11 @@
+import API from "./api";
+
+export async function getSemesters() {
+  const response = await API.get("/semester/");
+  return response.data;
+}
+
+export async function createSemester(data) {
+  const response = await API.post("/semester/", data);
+  return response.data;
+}
